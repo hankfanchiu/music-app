12724a8b964d61fb0ac11d0171c402b6fb52e11e
Hank Fanchiu <hank@fanchiu.com>
2015-11-19 14:10:05 -0800

    Create bands#index view

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,7 +1,7 @@
 class Album < ActiveRecord::Base
   RECORDING_TYPE = %w(live studio)
 
-  validates :name, :band_id, :recording_type, presence :true
+  validates :name, :band_id, :recording_type, presence: true
   validates :recording_type, inclusion: RECORDING_TYPE
 
   belongs_to :band
