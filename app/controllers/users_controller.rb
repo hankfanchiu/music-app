529d91158def08df30fbff14d5865d64800bddf4
Hank Fanchiu <hank@fanchiu.com>
2015-11-19 11:11:39 -0800

    Add Users controller and routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+    render :signup
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login_user!(@user)
+      redirect_to bands_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :signup
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
