d0a85279cc112a3924e927cd486daf4b457b3b04
Hank Fanchiu <hank@fanchiu.com>
2015-11-19 11:12:19 -0800

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: 'bands#index'
 
-  resource: :users, only: [:new, :create]
-  resource: :session, only: [:new, :create, :destroy]
-  resource: :bands do
+  resource :users, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
+  resource :bands do
   end
 end
