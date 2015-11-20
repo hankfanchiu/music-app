49df11d0040065b16fdab9db2998b32c70289b47
Hank Fanchiu <hank@fanchiu.com>
2015-11-19 16:58:31 -0800

    Add #ugly_lyrics helper_method

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,2 +1,5 @@
 module NotesHelper
+  def ugly_lyrics(lyrics)
+
+  end
 end
--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,10 @@
 module TracksHelper
+  include ERB::Util
+
+  def ugly_lyrics(lyrics)
+    lines = lyrics.split(/\n/)
+    ugly_lyrics = lines.map { |line| "<pre>&#9835; #{h(line)}</pre>" }
+
+    ugly_lyrics.html_safe
+  end
 end
